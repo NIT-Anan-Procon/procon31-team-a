436c83992bc092463a7d966ab26b612306268828
Taiki Orikami <ta.orikami@gmail.com>
2020-12-08 17:05:11 +0900

    様々なバグを修正しました。

--- calc.js
+++ calc.js
@@ -2,6 +2,7 @@ let numFl = 1 //数値フラグ
 let dotFl = 0 //小数点フラグ
 let opeFl = 0 //演算子フラグ
 let parCount = 0 //括弧カウント
+let minusFl = 0 //マイナスフラグ
 let firstFl = 0 //初入力フラグ
 let equalFl = 1 //演算処理フラグ
 let result = 0 //演算結果を入れる変数
@@ -9,45 +10,50 @@ let disp = '' //ディスプレイに表示する変数
 
 //数値 or 小数点が入力された時
 let inputNumber = value => {
-    if (equalFl == 1) { //演算後の入力をする時
-        inputClear();
-        equalFl = 0;
-    }
-    if (value == '.') { //小数点が入力されたとき
-        if (firstFl == 0) { //その項の最初の入力が小数点だった時
-            disp += '0.'; //0.○○
+    if ((disp.slice(disp.length - 1, disp.length) !== ')')) {
+        if (equalFl == 1) { //演算後の入力をする時
+            inputClear();
+            equalFl = 0;
+        }
+        if (value == '.') { //小数点が入力されたとき
+            if (firstFl == 0 && minusFl == 0) { //その項の最初の入力が小数点だった時
+                disp += '0.'; //0.○○
+                document.getElementById('d_box').textContent = disp;
+                console.log('0.');
+                numFl = 0;
+                opeFl = 0;
+                dotFl = 1;
+                firstFl = 1;
+                result = '0.'
+            }
+            else if (dotFl != 1 && minusFl == 0) { //その項で小数点がまだ無いとき
+                disp += '.'; //1~9.○○
+                document.getElementById('d_box').textContent = disp;
+                console.log('.');
+                numFl = 0;
+                opeFl = 0;
+                dotFl = 1;
+                result += '.';
+            }
+        } else if ((firstFl == 1 || (firstFl == 0 && value != '0' && value != '00')) && minusFl == 0) { //数値が入力された時
+            disp += value;
             document.getElementById('d_box').textContent = disp;
-            console.log('0.');
-            numFl = 0;
+            console.log(value);
+            numFl = 1;
             opeFl = 0;
-            dotFl = 1;
+            result += String(value);
+            if (firstFl == 0)
+                result = value;
             firstFl = 1;
-            result = '0.'
         }
-        else if (dotFl != 1) { //その項で小数点がまだ無いとき
-            disp += '.'; //1~9.○○
-            document.getElementById('d_box').textContent = disp;
-            console.log('.');
-            numFl = 0;
-            opeFl = 0;
-            dotFl = 1;
-            result += '.';
-        }
-    } else if (firstFl == 1 || value != '00') { //数値が入力された時
-        disp += value;
-        document.getElementById('d_box').textContent = disp;
-        console.log(value);
-        numFl = 1;
-        opeFl = 0;
-        firstFl = 1;
-        result += value;
     }
 }
 //演算子が入力された時
 let inputOperator = value => {
     if (equalFl == 1) { //演算後の値を利用する時
-        disp = String(result);
+        disp = result;
         document.getElementById('d_box').textContent = disp;
+        console.log(result);
         equalFl = 0;
     }
     if (numFl == 1 && opeFl == 0) {
@@ -57,7 +63,7 @@ let inputOperator = value => {
             console.log(value);
             numFl = 0;
             opeFl = 1;
-        } else if (value == '=') { //イコールが入力された時
+        } else if (value == '=' && String(eval(disp)) !== 'NaN') { //イコールが入力された時
             result = eval(disp);
             result = changeWord(result);
             disp = (disp + '=' + result);
@@ -69,8 +75,8 @@ let inputOperator = value => {
             equalFl = 1;
         }
         dotFl = 0;
+        minusFl = 0;
         firstFl = 0;
-        result = 0;
     }
 }
 //括弧が入力された時
@@ -91,14 +97,22 @@ let inputParenthesis = value => {
 }
 //+/-が入力された時
 let inputSign = () => {
+    if (equalFl == 0 && String(result).length == 1)
+        changeSign();
+    else if (equalFl == 0 && String(result).slice(String(result).length - 1, String(result).length) !== '.')
+        changeSign();
+}
+let changeSign = () => {
     if (result > 0) { //数値が正の時
         disp = disp.slice(0, disp.length - String(result).length);
         result = eval(-1 * result);
         disp += '(' + result + ')';
+        minusFl = 1;
     } else if (result < 0) { //数値が負の時
-        disp = disp.slice(0, disp.length - (String(result).length+2));
+        disp = disp.slice(0, disp.length - (String(result).length + 2));
         result = eval(-1 * result);
         disp += result;
+        minusFl = 0;
     }
     document.getElementById('d_box').textContent = disp;
     console.log(result);
@@ -109,6 +123,7 @@ let inputClear = () => {
     dotFl = 0;
     opeFl = 0;
     parCount = 0;
+    minusFl = 0;
     firstFl = 0;
     equalFl = 1;
     result = 0;
@@ -124,11 +139,9 @@ let inputDelete = () => {
         console.log('Delete');
     }
 }
-//演算時
+//演算結果チェック
 let changeWord = result => {
-    if (String(result) === 'NaN')
-        return '*文法Error(バグだと思ったらすぐご連絡ください)*';
-    else if (String(result) === 'Infinity')
+    if (String(result) === 'Infinity')
         return '*Infinity(仕様です)*'
     return result;
 }
